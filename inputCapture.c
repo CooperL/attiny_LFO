00ce152a7581bf32cd4f0cf225bc762831c00942
CooperL <cooper.loughlin@gmail.com>
2015-09-30 20:59:14 -0400

    nvm, tap only takes over when freq is really high for some reason

--- inputCapture.c
+++ inputCapture.c
@@ -9,8 +9,8 @@ void init_in_cap(unsigned long* tapAddr,
   DDRA &= ~(0b1<<PA3);
 
   // PORTA REGISTER
-  // PORTA3 = 1: PA3 get pullup resistor
-  PORTA |= (0b1<<PA3);
+  // PORTA3 = 0: no pullup resistor
+  PORTA &= ~(0b1<<PA3);
 
   // DATA DIRECTION REGISTER B
   // DEBUG -- DDRB2 = 1: PB2 is output to confirm switch input
@@ -33,8 +33,8 @@ void init_in_cap(unsigned long* tapAddr,
 // enable input capture interrupt
 void en_in_cap_interrupt(void) {
 	// MCU CONTROL REGISTER
-	// ISC01:0 = 10: interrupt triggered on falling edge
-	MCUCR = (0b10<<ISC00);
+	// ISC01:0 = 11: interrupt triggered on rising edge
+	MCUCR = (0b11<<ISC00);
 
 	// GENERAL INTERRUPT MASK REGISTER
 	// PCIE0 = 1: enable pin change 0 interrupt
